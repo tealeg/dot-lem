79f549aece24c2dc68d90482494695f45bbf8359
Geoffrey J. Teale <tealeg@gmail.com>
2022-05-03 14:38:38 +0200

    Make modeline-clock format and interval customisable.

--- modeline-clock.lisp
+++ modeline-clock.lisp
@@ -1,31 +1,26 @@
 (defpackage :lem-tealeg-modeline-clock
-  (:use :cl :lem)
+  (:use :cl :lem :local-time)
   (:export :enable
            :disable
            :modeline-clock-timer))
 
 (in-package :lem-tealeg-modeline-clock)
 
+(defvar lem-user::*modeline-clock-format*
+  '((:hour 2) ":" (:min 2) ":" (:sec 2) " " :year  "-" (:month 2) "-" (:day 2) " ")
+  "The format for displaying the time and/or date in the modeline. Expressed using the local-time library's syntax: https://github.com/dlowe-net/local-time")
+
+(defvar lem-user::*modeline-clock-refresh-interval*
+  1
+  "The time to wait, in seconds, between updates to the clock.  Defaults to 1.")
+
 (defun modeline-clock (window)
   (declare (ignore window))
-  (multiple-value-bind 
-        (second minute hour date month year day-number dst tz) (get-decoded-time)
-    (declare (ignore day-number)
-             (ignore dst)
-             (ignore tz))
-    (apply #'values (list (format nil "~2,'0d:~2,'0d:~2,'0d  ~d/~2,'0d/~d  "
-                          hour
-                          minute
-                          second
-                          date
-                          month
-                          year)
-                          nil
-                          :right))))
+  (apply #'values (list (local-time:format-timestring nil (local-time:now) :format lem-user::*modeline-clock-format*) nil :right)))
 
 (defvar modeline-clock-timer
   (sb-ext:make-timer #'lem:redraw-display)
-  "A timer to make the modeline redraw periodically, instaed of just on activity")
+  "A timer to make the modeline redraw periodically, instead of just on activity")
 
 (defun enable ()
   (sb-ext:schedule-timer modeline-clock-timer 1 :repeat-interval 1)
