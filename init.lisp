1949f6944248273db755cfb36cb946cdbaa6d986
Geoffrey J. Teale <tealeg@gmail.com>
2022-05-10 17:42:34 +0200

    use light theme

--- init.lisp
+++ init.lisp
@@ -9,14 +9,24 @@
 (define-key *global-keymap* "C-x u" 'undo)
 (setf *scroll-recenter-p* nil)
 
+
+
+
 (let ((asdf:*central-registry* (cons #P"~/.lem/" asdf:*central-registry*)))
   (ql:quickload :lem-tealeg-init)
 
   (ql:quickload :lem-tealeg-modeline-clock)
   (ql:quickload :lem-tealeg-org-mode)
+  
+  (ql:quickload :lem-modeline-battery)
 
-  (ql:quickload :lem-modeline-battery))
+  (ql:quickload :lem-paredit-mode)
+  )
 
 (lem-modeline-battery:enable)
 (lem-tealeg-modeline-clock:enable)
+
+(lem:load-theme "emacs-light")
+
+;; (add-hook lem-lisp-mode:*lisp-mode-hook* #'lem-paredit-mode:paredit-mode)
 
